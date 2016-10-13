7fa067d473a155d99560933926f2a06804a3b29e
Lndqw12 <COMP222012016@mail.ru>
2016-10-13 20:41:50 +0300

    static construct

---
+++ Join90.php
@@ -0,0 +1,26 @@
+﻿<?php
+class join{
+	public $a,$b;
+	private static $c;
+
+	function __construct($a=9,$b=3){
+		$this->a=$a;
+		$this->b=$b;
+	}
+	function sum(){
+		$this->c=$this->a*$this->b;
+	}
+	function vivod_c(){
+		return $this->c;	
+	}
+	function grin_v() {
+		$this->c=$this->c+24;
+	}
+	function __destruct(){
+		echo "Функция сработала";
+	}
+	    private static function abc() {
+        return self::$c;
+    }
+}
+?>
