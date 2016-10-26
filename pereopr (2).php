ba11d5e3b2798c05e35f0b50940563aaccd80e2f
Lndqw12 <COMP222012016@mail.ru>
2016-10-26 22:08:07 +0300

    phpAbstract

--- pereopr (2).php
+++ pereopr (2).php
@@ -1,15 +1,19 @@
-﻿<?php
+<?php
+require_once "AbstractClass.php";
+require_once "ConcreteClass1.php";
+require_once "ConcreteClass2.php";
+
 abstract class AbstractClass
 {
     abstract protected function getValue();
     abstract protected function prefixValue($prefix);
-
+    abstract public function f1();
     public function printOut() {
         print $this->getValue() . "\n";
     }
-	    public  function summa(){
-        $c = $this->f1();
-        $summa = $c*$c;
+    public  function sum(){
+        $n = $this->f1();
+        $sum = $n*$n;
         echo $sum;
     }
 }
