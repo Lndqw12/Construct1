4983bd029a647b5ff8a60f49b20851be9701bf92
Lndqw12 <COMP222012016@mail.ru>
2016-10-27 19:04:25 +0300

    27/10/2016/part1

---
+++ index0.php
@@ -0,0 +1,8 @@
+﻿<?php
+require_once "interface.php";
+	$obj = new Tools();
+	$obj->summa(9,9);
+	$itog = $obj->show();
+echo $rrt;
+echo $itog;
+?>
---
+++ interface.php
@@ -0,0 +1,24 @@
+﻿<?php
+interface Basainter
+{
+    public function summa($one,$two);
+    public function show();
+}
+class Tools implements Basainter
+{
+    public $res;
+    public function summa($one,$two)
+{
+        $this->res = $one+$two;
+		$this->res = $one*$two;
+}
+    public function show()
+{
+    return $this->res;
+}
+	function rrt($rrt){
+		  $this->rrt = $one+$two;
+	}
+}
+echo $rrt;
+?>
